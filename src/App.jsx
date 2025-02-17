b344b7471dc755e84f093b6ad3af239e16262a8c
Tristverm <emmanuelkyalo72@gmail.com>
2025-02-17 22:35:46 +0300

    Added chnage for re-render

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-const reactDescriptions = [`Fundamental`, `Crucial`, `Core`, `Essential`];
+import reactImg from "../src/assets/react-core-concepts.png";
+const reactDescriptions = [
+  `Fundamental`,
+  `Crucial`,
+  `Core`,
+  `Essential`,
+  `Clear`,
+  "Great",
+];
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
-let description = reactDescriptions[genRandomInt(reactDescriptions.length - 1)];
+
 const Header = () => {
+  let description =
+    reactDescriptions[genRandomInt(reactDescriptions.length - 1)];
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {`${description} React concepts you will need for almost any app you are
