c76d3cf846ee525e3e95b04b9e05c9640428aa27
Tristverm <emmanuelkyalo72@gmail.com>
2025-02-17 22:06:54 +0300

    Added a descrption variable

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ const reactDescriptions = [`Fundamental`, `Crucial`, `Core`, `Essential`];
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
+let description = reactDescriptions[genRandomInt(reactDescriptions.length - 1)];
 const Header = () => {
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        {`${
-          reactDescriptions[genRandomInt(reactDescriptions.length - 1)]
-        } React concepts you will need for almost any app you are
+        {`${description} React concepts you will need for almost any app you are
         going to build!`}
       </p>
     </header>
