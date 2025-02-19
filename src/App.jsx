4cb8628f277a2241fdc2ff23f153d4cb1a14a47c
Tristverm <emmanuelkyalo72@gmail.com>
2025-02-19 14:04:46 +0300

    Added a description of image deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import reactImg from "../src/assets/react-core-concepts.png";
+// we have to import this Image this way since it is stored in the src/assets folder
+// Images in this folder get optimized and bundled with different paths
+// This will enable the image to be rendered on deployment, as vite will keep track of the rlevant path on deployment.
 const reactDescriptions = [
   `Fundamental`,
   `Crucial`,
@@ -14,14 +17,14 @@ function genRandomInt(max) {
 const Header = () => {
   let description =
     reactDescriptions[genRandomInt(reactDescriptions.length - 1)];
-  console.log(reactImg);
+  console.log(description);
   return (
     <header>
       <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        {description}
-        React concepts you will need for almost any app you are going to build!
+        {description + " "}
+         React concepts you will need for almost any app you are going to build!
       </p>
     </header>
   );
